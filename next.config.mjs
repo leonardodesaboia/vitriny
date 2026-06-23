d65f5ca7caca16f9ffa871fd4fcd6583a3c6dc9f
LEONARDO DE SABOIA CORREA PONTE SOUZA <leonardodesaboia@hotmail.com>
2026-06-23 18:20:06 -0300

    chore: remove arquivo ECC e desativa devIndicators

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,8 @@ const projectRoot = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: projectRoot
+  outputFileTracingRoot: projectRoot,
+  devIndicators: false
 };
 
 export default nextConfig;
